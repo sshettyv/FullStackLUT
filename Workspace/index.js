30e90ac0b1a88e5ea10ec9e4b1f474eeeb47a7d9
vijeth shetty <vijethcshetty@gmail.com>
2023-11-13 21:43:36 +0200

    learning diary date 13.11
    learning diary date 13.11

--- Workspace/index.js
+++ Workspace/index.js
@@ -1,9 +1,77 @@
-const Logger = require ('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req,res)=>{
 
-logger.on('message', (data) => console.log('Called Listener' ,data  ));
 
-logger.log('Hello World 1  ');
-logger.log('Hello World  2 ');
-logger.log('Hello World  3 ');
+   /*  if(req.url === '/'){
+        fs.readFile(path.join(__dirname,'public','index.html'),(err,content) => {
+            if(err) throw err ; 
+            res.writeHead (200, {'Content-Type': 'text/html'});
+            res.end(content);
+        } );
+     }
+     else if(req.url === '/about'){
+        fs.readFile(path.join(__dirname,'public','about.html'),(err,content) => {
+            if(err) throw err ; 
+            res.writeHead (200, {'Content-Type': 'text/html'});
+            res.end(content);
+        }
+        );
+    } */
+
+    let filePath = path.join( __dirname, "public", req.url === "/" ? "index.html" : req.url );
+
+    let extname = path.extname(filePath);
+    let contentType = "text/html";
+    if (contentType == "text/html" && extname == "") filePath += ".html";
+
+    switch (extname) {
+        case ".js":
+          contentType = "text/javascript";
+          break;
+        case ".css":
+          contentType = "text/css";
+          break;
+        case ".json":
+          contentType = "application/json";
+          break;
+        case ".png":
+          contentType = "image/png";
+          break;
+        case ".jpg":
+          contentType = "image/jpg";
+          break;
+      }
+
+      fs.readFile(filePath, (err, content) => {
+        if (err) {
+          if (err.code == "ENOENT") {
+            // Page not found
+            fs.readFile(
+              path.join(__dirname, "public", "404.html"),
+              (err, content) => {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.end(content, "utf8");
+              }
+            );
+          } else {
+            //  Some server error
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
+          }
+        } else {
+          // Success
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(content, "utf8");
+        }
+      });
+
+
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
--- Workspace/logger.js
+++ Workspace/logger.js
@@ -9,4 +9,16 @@ class Logger extends EventEmitter{
     }
 }
 
-module.exports = Logger ; 
+//module.exports = Logger ; 
+
+
+
+const Logger = require ('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener' ,data  ));
+
+logger.log('Hello World 1  ');
+logger.log('Hello World  2 ');
+logger.log('Hello World  3 ');
